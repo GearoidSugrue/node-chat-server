79cf5bc9edc656f421ff9c0221ff5f334f01d5bd
Gearoid Sugrue <gearoid.sugrue@outlook.com>
2019-06-05 13:42:00 +0700

    Add temp data stores and utils for users and chatrooms

---
+++ src/data-store/chatroom-store.ts
@@ -0,0 +1,178 @@
+import { Chatroom } from '../types/chatroom.type';
+import { Message } from '../types/message.type';
+
+const dummyMessages = [
+  {
+    userId: 'zzzzz-zzzzz-zzzzz',
+    username: 'Narrator',
+    message: `No one would have believed
+      in the last years of the 19th century
+      that human affairs where being watched
+      by intelligences that inhabited the timeless worlds of space.
+      No one could have dreamed we were being scrutinized
+      as someone with a microscope studies creatures
+      that swarm and multiply in a drop of water.
+      Few men even considered the possibility of life on other planets
+      and yet, across the gulf of space
+      minds immeasurably superior to ours
+      regarded this Earth with envious eyes,
+      and slowly and surely
+      they drew their plans against us.`
+  },
+  {
+    userId: 'aaaaa-bbbbb-cccc',
+    username: 'Winston',
+    message:
+      'The chances of anything coming from Mars are a million to one! But still they come! '
+  },
+  {
+    userId: 'ggggg-hhhhh-iiiiiii',
+    username: 'Nathaniel',
+    message: `Listen, do you hear them drawing near,
+      In their search for the sinners?
+      Feeding on the power of our fear,
+      And the evil within us.
+      Incarnation of Satan's creation of all that we dread,
+      When the demons arrive,
+      Those alive would be better off dead`
+  },
+  {
+    userId: 'aaaaa-bbbbb-cccc',
+    username: 'Winston',
+    message: `Be a man!... 
+      What good is religion if it collapses under calamity? 
+      Think of what earthquakes and floods, wars and volcanoes, have done before to men! 
+      Did you think that God had exempted us? He is not an insurance agent.`
+  },
+  {
+    userId: 'ggggg-hhhhh-iiiiiii',
+    username: 'Artilleryman',
+    message:
+      "This isn't a war. It never was a war, any more than there's war between man and ants."
+  },
+  {
+    userId: 'aaaaa-bbbbb-cccc',
+    username: 'Winston',
+    message: `We'll peck them to death tomorrow dear.`
+  }
+];
+
+// holds user and chatroom data. Hard-coded for now but will use some DB in future.
+export class ChatroomsStore {
+  public readonly chatrooms: { [key: string]: Chatroom };
+
+  constructor() {
+    console.log('Initializing Chatrooms Data Store...');
+
+    this.chatrooms = {
+      '1111-2222-33333': {
+        chatroomId: '1111-2222-33333',
+        name: 'New Users Chat!',
+        memberIds: ['fiendishly-handsome-fellow'],
+        messages: [
+          {
+            userId: 'fiendishly-handsome-fellow',
+            username: 'Gary',
+            message: `We'll peck them to death tomorrow dear.`
+          }
+        ]
+      },
+      '9999-8888-77777': {
+        chatroomId: '9999-8888-77777',
+        name: 'War of the Worlds Discussion',
+        memberIds: [
+          'zzzzz-zzzzz-zzzzz',
+          'aaaaa-bbbbb-cccc',
+          'ggggg-hhhhh-iiiiiii'
+        ],
+        messages: dummyMessages
+      }
+    };
+    console.log('Chatrooms Data Store Initialized!');
+  }
+
+  public getChatroom(chatroomId: string): Chatroom {
+    return this.chatrooms[chatroomId] || ({} as Chatroom);
+  }
+
+  public getChatrooms(): Chatroom[] {
+    console.log('Getting all users');
+    return Object.values(this.chatrooms);
+  }
+
+  public getChatroomMessages(
+    chatroomId: string,
+    requesterUserId: string
+  ): Message[] {
+    const chatroom = this.getChatroom(chatroomId);
+    const isUserMember =
+      chatroom &&
+      chatroom.memberIds &&
+      chatroom.memberIds.includes(requesterUserId);
+
+    if (!isUserMember) {
+      console.log('Unauthorized attempt to view chatroom messages', {
+        chatroomId,
+        requesterUserId
+      });
+      return [];
+    }
+    const messages = chatroom.messages || [];
+
+    // todo delete me!
+    const testData: Message[] = [
+      {
+        userId: 'fiendishly-handsome-fellow',
+        username: 'Gary',
+        message: `Test message in chatroom: ${chatroomId}  -  ${Math.random() *
+          100000000}`
+      }
+    ];
+    return [...messages, ...testData];
+  }
+
+  // public addChatroom(chatroom: Chatroom): void {
+  public addChatroom(name: string): Chatroom {
+    const validChatroom = name && typeof name === 'string';
+
+    if (!validChatroom) {
+      console.log('Invalid attempt to create Chatroom', { name });
+      return;
+    }
+
+    const chatroomId = 'todo-generate-chatroomId'; // todo generate uuid here
+    this.chatrooms[chatroomId] = {
+      name,
+      chatroomId,
+      memberIds: [], // todo add userId of creator here?
+      messages: [] // todo add default user create message here?
+    };
+    return this.chatrooms[chatroomId];
+  }
+
+  public updateChatroomDetails(
+    chatroomId: string,
+    updatedDetails: Partial<Chatroom>
+  ): Chatroom {
+    const currentDetails = this.chatrooms[chatroomId];
+    const updatedChatroom = {
+      ...currentDetails,
+      ...updatedDetails,
+      chatroomId // since the chatroomId is the chatrooms Map/Object Key I'm preventing it from being changed
+    };
+    this.chatrooms[chatroomId] = updatedChatroom;
+    return updatedChatroom;
+  }
+
+  public addMemberToChatroom(chatroomId: string, userId: string): void {
+    const currentMembersIds = this.getChatroom(chatroomId).memberIds || [];
+    const updatedMembersIds = [...currentMembersIds, userId];
+    this.updateChatroomDetails(chatroomId, { memberIds: updatedMembersIds });
+  }
+
+  public addMessageToChatroom(chatroomId: string, message: Message): void {
+    const currentMessages = this.getChatroom(chatroomId).messages || [];
+    const updatedMessages = [...currentMessages, message];
+    this.updateChatroomDetails(chatroomId, { messages: updatedMessages });
+  }
+}
---
+++ src/data-store/users-store.ts
@@ -0,0 +1,133 @@
+import { Message } from '../types/message.type';
+import { User } from '../types/user.type';
+
+const dummyUsers = {
+  'zzzzz-zzzzz-zzzzz': {
+    userId: 'zzzzz-zzzzz-zzzzz',
+    username: 'Narrator',
+    online: false,
+    messages: {}
+  },
+  'aaaaa-bbbbb-cccc': {
+    userId: 'aaaaa-bbbbb-cccc',
+    username: 'Winston',
+    online: false,
+    messages: {
+      'ddddd-eeeee-fffff': [
+        {
+          userId: 'ddddd-eeeee-fffff',
+          username: 'Nathaniel',
+          message: 'Hello!'
+        }
+      ]
+    }
+  },
+  'ddddd-eeeee-fffff': {
+    userId: 'ddddd-eeeee-fffff',
+    username: 'Nathaniel',
+    online: true,
+    messages: {}
+  },
+  'ggggg-hhhhh-iiiiiii': {
+    userId: 'ggggg-hhhhh-iiiiiii',
+    username: 'Artilleryman',
+    online: false,
+    messages: {}
+  },
+  'fiendishly-handsome-fellow': {
+    userId: 'fiendishly-handsome-fellow',
+    username: 'Gary the Great',
+    online: false,
+    messages: {}
+  }
+};
+
+// holds user and chatroom data. Hard-coded for now but will use some DB in future.
+export class UsersStore {
+  public readonly users: { [key: string]: User };
+
+  constructor() {
+    console.log('Initializing Users Data Store...');
+    this.users = dummyUsers;
+    console.log('Users Data Store Initialized!');
+  }
+
+  public getUser(userId: string): User {
+    return this.users[userId] || ({} as User);
+  }
+
+  public getUserMessages(userId: string, requestingUserId: string): Message[] {
+    const user = this.getUser(userId);
+
+    if (!user || !requestingUserId) {
+      console.log('Invalid attempt to get user messages', {
+        userId,
+        requestingUserId
+      });
+      return [];
+    }
+
+    const userMessages = user.messages || {};
+    return userMessages[requestingUserId] || [];
+  }
+
+  public getUsers(): User[] {
+    console.log('Getting all users');
+    return Object.values(this.users);
+  }
+
+  public addUser(username: string): User {
+    const validUser = username && typeof username === 'string';
+
+    if (!validUser) {
+      console.log('Invalid attempt to create User', { username });
+      return;
+    }
+
+    const userId = 'todo-generate-uuid';
+    this.users[userId] = {
+      userId,
+      username,
+      messages: {}
+    };
+    return this.users[userId];
+  }
+
+  public updateUserDetails(
+    userId: string,
+    updatedDetails: Partial<User>
+  ): User {
+    const currentDetails = this.users[userId];
+    const updatedUser = {
+      ...currentDetails,
+      ...updatedDetails,
+      userId // since the userId is the users Map/Object Key I'm preventing it from being changed
+    };
+    this.users[userId] = updatedUser;
+    return updatedUser;
+  }
+
+  public addMessageToUser(
+    userId: string,
+    fromUserId: string,
+    message: Message
+  ): void {
+    const isValid = fromUserId && userId && this.users[userId];
+
+    if (!isValid) {
+      console.log('Invalid attempt to add message:', {
+        userId,
+        fromUserId,
+        message
+      });
+      return;
+    }
+    const currentUserMessages = this.users[userId].messages[fromUserId];
+    const updatedUserMessages = [...currentUserMessages, message];
+    this.users[userId].messages[fromUserId] = updatedUserMessages;
+  }
+
+  public doesUserExist(userId: string): boolean {
+    return Boolean(this.getUser(userId));
+  }
+}
